f0a06a3f2cbecc8497c10066414fcbba41c81833
Bruce <bruce@yogainternational.com>
2019-04-16 15:28:46 -0400

    updates setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
+module.exports = (options) => {
+  return {
+    entry: './src/app.jsx',
+    output: {
+      filename: 'bundle.js',
+    },
+  };
 };
