8f9c1ffdc1cceb1fe661e7f0b766013adf3e6f0a
Bruce <bruce@yogainternational.com>
2019-04-16 16:33:52 -0400

    adds code for React input and buttons

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Section from './Section.jsx';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      inputText: '',
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  addSection(event) {
+    this.setState(() => {});
+  }
+
+  onChange(event) {
+    const value = event.target.value;
+    this.setState(() => ({ inputText: value }));
+    event.target.value = this.state[event.target.name];
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <div>
+          <input
+            style={{
+              display: 'block',
+              fontSize: '20px',
+              margin: 'auto',
+              marginBottom: '10px',
+            }}
+            value={this.state.inputText}
+            onChange={this.onChange}
+            placeholder="Header or section"
+          />
+          <button
+            style={{
+              fontSize: '15px',
+              cursor: 'pointer',
+              borderRadius: '10px',
+              padding: '10px',
+              margin: '5px',
+            }}
+          >
+            New Header
+          </button>
+          <button
+            style={{
+              fontSize: '15px',
+              cursor: 'pointer',
+              borderRadius: '10px',
+              padding: '10px',
+              margin: '5px',
+            }}
+          >
+            New Section
+          </button>
+        </div>
+        <div>{this.state.items.map((item) => item)}</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
-module.exports = (options) => {
+module.exports = () => {
   return {
     entry: './src/app.jsx',
     output: {
       filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
     },
   };
 };
