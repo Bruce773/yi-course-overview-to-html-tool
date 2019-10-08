58e8e75ba1b0cafebc9f1934955bd023f8c2d8dc
Bruce <bruce@yogainternational.com>
2019-10-08 12:17:07 -0400

    removes Delete

--- src/LayoutComponents/Header.jsx
+++ src/LayoutComponents/Header.jsx
@@ -5,6 +5,6 @@ let key = 0;
 
 export const Header = ({ text, deleteHeader }) => (
   <h2 id={key++} style={{ fontSize: '30px', padding: '0px', margin: '0px' }}>
-    + {text} <Delete onClick={() => deleteHeader(key)}>Delete</Delete>
+    + {text}
   </h2>
 );
